9020b9fae4914b31db2347659d0e74768345634c
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-21 12:42:11 +0300

    Create soical_elements.java

---
+++ soical_elements.java
@@ -0,0 +1,22 @@
+//your search scripts from clients.social.soicalmedia//
+//created By Blenguar Inc. //
+//api samples//
+ 
+import.os  
+import.com.cilent.soical.socialmedia
+import.public.cilent.soicalmedia
+import.programmingextension
+  @Ovveride
+     try{
+      cilent.ext
+         public.processor
+  @Ovveride
+    print(socialelement).language
+      
+   //Blocks//
+    print.block(soicalelement.socialview)
+    this.block(element.processor
+          processor.element
+     try 
+          processor.social(material.view)
+ 
